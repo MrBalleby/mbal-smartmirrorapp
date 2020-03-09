1a5b35f2fff8e170f2e3c7b4aaf21f6afc604c4b
Mark Balleby Hansen <mbal@verdo.com>
2020-03-09 15:21:55 +0100

    Added Project Added Layout Added Spotify Api

---
+++ SmartMirror Webapplication/SmartMirror/wwwroot/JS/weatherScript.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $(function () {
+        $('#LocationBtn').on('click', function () {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "https://api.weatherbit.io/v2.0/current&city=" & document.getElementById('CityName').value & "&country=" & document.getElementById('CountryName').value &"&key=3abb6702d74e418db1676a60d34ad74e",
+                success: function (data) {
+                    $('#MediaTitle').empty();
+                    $('#Date').empty();
+                    $('#Explanation').empty();
+                    $('#NasaLink').empty();
+
+                    $('#MediaTitle').append(JSON.stringify(data.title));
+                    $('#Date').append(JSON.stringify(data.date));
+                    $('#Explanation').append(JSON.stringify(data.explanation));
+                    $('#NasaLink').append(JSON.stringify(data.url));
+                    switch (data.media_type) {
+                        case 'image':
+                            $('#Picture').attr('src', data.url);
+                            $('#Frame').hide();
+                            break;
+                        case 'video':
+                            $('#Frame').attr('src', data.url);
+                            $('#Picture').hide();
+                            break;
+                        default:
+                    }
+                }
+            });
+        });
+
+    });
+});
