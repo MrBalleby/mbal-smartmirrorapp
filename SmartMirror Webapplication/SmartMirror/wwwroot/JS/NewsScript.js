743842fa40951b737911865125cab55670245208
Counterlesscount <mbal@verdo.com>
2020-03-10 23:45:46 +0100

    Work on NewsApi

---
+++ SmartMirror Webapplication/SmartMirror/wwwroot/JS/NewsScript.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(function () {
+        $('#LocationBtn').on('click', function () {
+            console.log('in function');
+            var searchterm = document.getElementById("SearchTerm").value;
+            var myurl = "http://newsapi.org/v2/top-headlines?q=" + searchterm + "&apiKey=991438f455f34681a3350955b789bc0c";
+            console.log(myurl);
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: myurl,
+                success: function (data) {
+                    console.log("success");
+                    for (a in data.articles) {
+
+                    }
+                }
+            });
+        });
+    });
+});
